1b79cac089ff7f90576ab8fad414ec6a74e321ca
achmichael <achmadmichael4@gmail.com>
2024-06-20 23:50:10 +0700

    Update Access Validation

--- src/Middleware/accessValidation.js
+++ src/Middleware/accessValidation.js
@@ -12,6 +12,12 @@ export function accessValidation(req, res, next){
         req.user = decoded;
         next();
     }catch(error){
-        return next(new ResponseError(500, "Token Is Expired"));
+        if (error.name === 'TokenExpiredError') {
+            return next(new ResponseError(500, "Token Is Expired"));
+        } else if (error.name === 'JsonWebTokenError') {
+            return next(new ResponseError(401, "Invalid Token"));
+        } else {
+            return next(new ResponseError(500, "Token Verification Failed"));
+        }
     }
 }
